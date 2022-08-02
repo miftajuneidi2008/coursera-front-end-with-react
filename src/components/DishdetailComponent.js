ada3e8422f8d3bb0de13bafa085cf97e0f7e9c53
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2022-08-02 15:38:30 +0300

    'presentational-and-container-components'

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -11,7 +11,8 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
     {
        if (this.props.dish != null)
             return(
-                <>
+                <div className="container">
+                    <div className="row">
                 <div  className="col-12 col-md-5 m-1">
                 <Card>
                     <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
@@ -30,18 +31,19 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,CardTitle } from 'rea
 
                             <ul className="list-unstyled" key={id}>
                                 <li>{comment}</li>
-                                <li>--{author},{date}</li>
+                                <li>--{author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(date)))}</li>
                             </ul>
                             
                         )
                     })}
 
                 </div>
-                </>
+                </div>
+                </div>
             );
         else
             return(
-                <div>cbvnbvcn</div>
+                <div></div>
             );
     }
  }
