a8ff94ea451805cf8391b223606b23682b43e028
miftajuneidi2008 <miftajuneidi2008@gmail.com>
2022-09-12 14:48:51 +0300

    Combining Reducers

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardImg,
@@ -8,9 +8,19 @@ import {
   CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Button,
+  Label,
+  Row,
+  Col,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
-
+const required = (val) => val && val.length; //value > 0
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
 function RenderDish({ dish }) {
   return (
     <div className="col-12 col-md-5 m-1">
@@ -25,7 +35,13 @@ function RenderDish({ dish }) {
   );
 }
 
+function handleCommentFormSubmit(values) {
+  console.log("Current State is: " + JSON.stringify(values));
+  alert("Current State is: " + JSON.stringify(values));
+}
 function RenderComments({ comments }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -46,6 +62,113 @@ function RenderComments({ comments }) {
             </ul>
           );
         })}
+        <Button outline onClick={() => setIsModalOpen(!isModalOpen)}>
+          <span className="fa fa-pencil"></span> Submit Comment
+        </Button>
+        <Modal isOpen={isModalOpen} toggle={() => setIsModalOpen(!isModalOpen)}>
+          <ModalHeader toggle={() => setIsModalOpen(!isModalOpen)}>
+            Submit Comment
+          </ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => handleCommentFormSubmit(values)}>
+              <Row className="form-group">
+                <Label htmlFor="rating" md={12}>
+                  Rating
+                </Label>
+                <Col md={12}>
+                  <Control.select
+                    model=".rating"
+                    className="form-control"
+                    name="rating"
+                    id="rating"
+                    validators={{
+                      required,
+                    }}
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Control.select>
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                    }}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="author" md={12}>
+                  {" "}
+                  Your Name{" "}
+                </Label>
+                <Col md={12}>
+                  <Control.text
+                    model=".author"
+                    id="author"
+                    name="author"
+                    placeholder="First Name"
+                    className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less",
+                    }}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="comment" md={12}>
+                  Comment
+                </Label>
+                <Col md={12}>
+                  <Control.textarea
+                    model=".comment"
+                    id="comment"
+                    name="comment"
+                    rows="6"
+                    className="form-control"
+                    validators={{
+                      required,
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                    }}
+                  />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Col>
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
       </div>
     );
   } else {
---
+++ src/redux/comments.js
@@ -0,0 +1,7 @@
+import { COMMENTS } from "../shared/Comments";
+export const Comments = (state = COMMENTS, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
